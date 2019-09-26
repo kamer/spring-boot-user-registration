84e9a89023654239d819fda0ce87106924538b6f
Kamer <kamer@kamerelciyar.com>
2019-09-26 23:47:26 +0300

    Initial commit

---
+++ src/main/java/com/kamer/springbootuserregistration/SpringBootUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.kamer.springbootuserregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamer/springbootuserregistration/SpringBootUserRegistrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamer.springbootuserregistration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootUserRegistrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
